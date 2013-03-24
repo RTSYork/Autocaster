736d4b709da26f40161b836d3603247126147c04
Russell Joyce <russell@russellj.co.uk>
2013-03-24 15:17:07 +0000

    Added UARTImage project

---
+++ UARTImage/UARTImage/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace UARTImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.framesTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.frameTrackBar = new System.Windows.Forms.TrackBar();
+            this.frameLabel = new System.Windows.Forms.Label();
+            this.saveAllButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 460800;
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.InitialDirectory = "h:\\Project\\Screenshots";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Log File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(643, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Serial";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Save Frame";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.InitialDirectory = "h:\\Project\\Screenshots";
+            this.saveFileDialog1.Title = "Save Frame as Bitmap Image";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(348, 14);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(43, 20);
+            this.widthTextBox.TabIndex = 6;
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(415, 14);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(43, 20);
+            this.heightTextBox.TabIndex = 7;
+            // 
+            // framesTextBox
+            // 
+            this.framesTextBox.Location = new System.Drawing.Point(578, 13);
+            this.framesTextBox.Name = "framesTextBox";
+            this.framesTextBox.Size = new System.Drawing.Size(36, 20);
+            this.framesTextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Capture size:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(479, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Number of frames:";
+            // 
+            // frameTrackBar
+            // 
+            this.frameTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.frameTrackBar.Location = new System.Drawing.Point(747, 11);
+            this.frameTrackBar.Maximum = 0;
+            this.frameTrackBar.Name = "frameTrackBar";
+            this.frameTrackBar.Size = new System.Drawing.Size(489, 45);
+            this.frameTrackBar.TabIndex = 12;
+            this.frameTrackBar.Scroll += new System.EventHandler(this.frameTrackBar_Scroll);
+            // 
+            // frameLabel
+            // 
+            this.frameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.frameLabel.Location = new System.Drawing.Point(1235, 13);
+            this.frameLabel.Name = "frameLabel";
+            this.frameLabel.Size = new System.Drawing.Size(55, 17);
+            this.frameLabel.TabIndex = 13;
+            this.frameLabel.Text = "00 / 00";
+            this.frameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // saveAllButton
+            // 
+            this.saveAllButton.Location = new System.Drawing.Point(186, 12);
+            this.saveAllButton.Name = "saveAllButton";
+            this.saveAllButton.Size = new System.Drawing.Size(75, 23);
+            this.saveAllButton.TabIndex = 14;
+            this.saveAllButton.Text = "Save All";
+            this.saveAllButton.UseVisualStyleBackColor = true;
+            this.saveAllButton.Click += new System.EventHandler(this.saveAllButton_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "h:\\Project\\Screenshots";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 775);
+            this.Controls.Add(this.saveAllButton);
+            this.Controls.Add(this.frameLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.framesTextBox);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.frameTrackBar);
+            this.Name = "Form1";
+            this.Text = "Serial Screenshot Grabber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.TextBox framesTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar frameTrackBar;
+        private System.Windows.Forms.Label frameLabel;
+        private System.Windows.Forms.Button saveAllButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ UARTImage/UARTImage/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UARTImage
+{
+    public partial class Form1 : Form
+    {
+        int frameWidth = 414;
+        int frameHeight = 50;
+        int frameCount = 60;
+
+        Bitmap[] b;
+        int x;
+        int y;
+        int z;
+        byte[] bytes;
+        int i;
+
+        int currentFrame = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            widthTextBox.Text = frameWidth.ToString();
+            heightTextBox.Text = frameHeight.ToString();
+            framesTextBox.Text = frameCount.ToString();
+
+            pictureBox1.Width = frameWidth;
+            pictureBox1.Height = frameHeight;
+
+            frameTrackBar.Maximum = frameCount - 1;
+            frameLabel.Text = (currentFrame + 1) + " / " + frameCount;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                b = new Bitmap[1];
+
+                b[0] = new Bitmap(frameWidth, frameHeight);
+                i = 0;
+
+                FileStream fs = (FileStream)openFileDialog1.OpenFile();
+                try
+                {
+                    bytes = new byte[fs.Length];
+                    fs.Read(bytes, 0, Convert.ToInt32(fs.Length));
+                    fs.Close();
+                }
+                finally
+                {
+                    fs.Close();
+                }
+
+                for (int y = 0; y < frameHeight; y++)
+                {
+                    for (int x = 0; x < frameWidth; x++)
+                    {
+                        if (i > bytes.Length - 3)
+                            break;
+
+                        b[0].SetPixel(x, y, Color.FromArgb(bytes[i], bytes[i+1], bytes[i+2]));
+                        i += 3;
+                    }
+                }
+
+                pictureBox1.Image = b[0];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                frameWidth = int.Parse(widthTextBox.Text);
+                frameHeight = int.Parse(heightTextBox.Text);
+                frameCount = int.Parse(framesTextBox.Text);
+
+                pictureBox1.Width = frameWidth;
+                pictureBox1.Height = frameHeight;
+
+                currentFrame = 0;
+                frameTrackBar.Value = 0;
+                frameTrackBar.Maximum = frameCount - 1;
+                frameLabel.Text = (currentFrame + 1) + " / " + frameCount;
+
+                button2.Text = "Stop Serial";
+                serialPort1.Open();
+
+                b = new Bitmap[frameCount];
+
+                for (int a = 0; a < frameCount; a++)
+                {
+                    b[a] = new Bitmap(frameWidth, frameHeight);
+                }
+
+                x = 0;
+                y = 0;
+                z = 0;
+                i = 0;
+                bytes = new byte[3];
+                timer1.Start();
+            }
+            else
+            {
+                button2.Text = "Start Serial";
+                serialPort1.Close();
+                timer1.Stop();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                while (serialPort1.BytesToRead > 0)
+                {
+                    bytes[i] = (byte)serialPort1.ReadByte();
+
+                    if (y == frameHeight && z < frameCount - 1)
+                    {
+                        y = 0;
+                        z++;
+
+                        currentFrame = z;
+                        //frameTrackBar.Value = z;
+                        //frameLabel.Text = (currentFrame + 1) + " / " + frameCount;
+                    }
+
+                    if (y < frameHeight)
+                    {
+                        if (i == 2)
+                        {
+                            Monitor.Enter(b[z]);
+                            b[z].SetPixel(x, y, Color.FromArgb(bytes[0], bytes[1], bytes[2]));
+                            Monitor.Exit(b[z]);
+
+                            x++;
+                            if (x == frameWidth)
+                            {
+                                x = 0;
+                                y++;
+                            }
+
+                            i = 0;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        serialPort1.ReadByte();
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Monitor.Enter(b[currentFrame]);
+            pictureBox1.Image = (Image)b[currentFrame].Clone();
+            Monitor.Exit(b[currentFrame]);
+
+            frameTrackBar.Value = currentFrame;
+            frameLabel.Text = (currentFrame + 1) + " / " + frameCount;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                b[currentFrame].Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void frameTrackBar_Scroll(object sender, EventArgs e)
+        {
+            currentFrame = frameTrackBar.Value;
+            frameLabel.Text = (currentFrame + 1) + " / " + frameCount;
+
+            Monitor.Enter(b[currentFrame]);
+            pictureBox1.Image = (Image)b[currentFrame].Clone();
+            Monitor.Exit(b[currentFrame]);
+        }
+
+        private void saveAllButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                for (int a = 0; a < b.Length; a++)
+                {
+                    String fileName = folderBrowserDialog1.SelectedPath + "\\frame" + (a+1) + ".png";
+                    b[a].Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+        }
+
+    }
+}
