9505737b27d092cddeda83446f91f2788cb076f5
Russell Joyce <russell@russellj.co.uk>
2013-08-12 15:56:44 +0100

    Teensy: Updated input pins ready for new PCB
    Removed pins that aren't actually used

--- Teensy/Guitar/Guitar.ino
+++ Teensy/Guitar/Guitar.ino
@@ -11,24 +11,21 @@ int whammyValue = 128;
  *  2 - Yellow Fret
  *  3 - Blue Fret
  *  4 - Orange Fret
- *  5 - Strum Bar
- *  6 - Up
- *  7 - Right
- *  8 - Down
- *  9 - Left
- * 10 - Start Button
- * 11 - Select Button
- * 12 - PS Button
+ *  5 - Up
+ *  6 - Right
+ *  7 - Down
+ *  8 - Left
+ *  9 - Start Button
+ * 10 - Select Button
  * 13 - LED (on board)
- * 14 - Tilt Sensor
- * 15 - Whammy Bar 0 (LSB)
- * 16 - Whammy Bar 1
- * 17 - Whammy Bar 2
- * 18 - Whammy Bar 3
- * 19 - Whammy Bar 4
- * 20 - Whammy Bar 5
- * 21 - Whammy Bar 6
- * 22 - Whammy Bar 7 (MSB)
+ * 14 - Whammy Bar 0 (LSB)
+ * 15 - Whammy Bar 1
+ * 16 - Whammy Bar 2
+ * 17 - Whammy Bar 3
+ * 18 - Whammy Bar 4
+ * 19 - Whammy Bar 5
+ * 20 - Whammy Bar 6
+ * 21 - Whammy Bar 7 (MSB)
  **/
 
 void setup() {
@@ -43,7 +40,6 @@ void setup() {
   
   // Initialise Guitar interface
   Guitar.useManualSend(true);
-  //Guitar.setExtras();
   Guitar.position(128, 128);
   Guitar.Zrotate(128);
   Guitar.whammy(whammyValue);
@@ -54,7 +50,7 @@ void setup() {
   
   Guitar.send_now();
   
-  // Light LED
+  // Flash and light LED
   digitalWrite(ledPin, HIGH);
   delay(250);
   digitalWrite(ledPin, LOW);
@@ -69,47 +65,45 @@ void setup() {
 
 void loop() {
   
+  // Fret buttons
   Guitar.fret(GREEN,  digitalRead(0));
   Guitar.fret(RED,    digitalRead(1));
   Guitar.fret(YELLOW, digitalRead(2));
   Guitar.fret(BLUE,   digitalRead(3));
   Guitar.fret(ORANGE, digitalRead(4));
   
-  if(digitalRead(5))
-    Guitar.strum(true);
-  else if (digitalRead(6))
+  // Directional buttons (and strum)
+  if (digitalRead(5))
     Guitar.hat(0);
-  else if (digitalRead(7))
+  else if (digitalRead(6))
     Guitar.hat(90);
-  else if (digitalRead(8))
+  else if (digitalRead(7))
     Guitar.hat(180);
-  else if (digitalRead(9))
+  else if (digitalRead(8))
     Guitar.hat(270);
   else
     Guitar.strum(false);
   
-  Guitar.button(START,  digitalRead(10));
-  Guitar.button(SELECT, digitalRead(11));
-  //Guitar.button(PS,     digitalRead(12));
+  // Start and Select (Tilt) buttons
+  Guitar.button(START,  digitalRead(9));
+  Guitar.button(SELECT, digitalRead(10));
   
-  //Guitar.tilt(digitalRead(14));
-  
-  whammyValue = (!digitalRead(15) ? 0 : 1)        +
-                (!digitalRead(16) ? 0 : (1 << 1)) +
-                (!digitalRead(17) ? 0 : (1 << 2)) +
-                (!digitalRead(18) ? 0 : (1 << 3)) +
-                (!digitalRead(19) ? 0 : (1 << 4)) +
-                (!digitalRead(20) ? 0 : (1 << 5)) +
-                (!digitalRead(21) ? 0 : (1 << 6)) +
-                (!digitalRead(22) ? 0 : (1 << 7));
-  
+  // Whammy bar
+  whammyValue = (!digitalRead(14) ? 0 : 1)        +
+                (!digitalRead(15) ? 0 : (1 << 1)) +
+                (!digitalRead(16) ? 0 : (1 << 2)) +
+                (!digitalRead(17) ? 0 : (1 << 3)) +
+                (!digitalRead(18) ? 0 : (1 << 4)) +
+                (!digitalRead(19) ? 0 : (1 << 5)) +
+                (!digitalRead(20) ? 0 : (1 << 6)) +
+                (!digitalRead(21) ? 0 : (1 << 7));
+                
   Guitar.whammy(whammyValue);
   
   
   // Send data
   Guitar.send_now();
   
-  // a brief delay, so this runs "only" 200 times per second
+  // Brief delay, so this runs ~200 times per second
   delay(5);
 }
-
