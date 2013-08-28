8bd386bb04378c5549c974ff797921acaaf23112
Russell Joyce <russell@russellj.co.uk>
2013-08-28 11:00:44 +0100

    SDK: Fixed options header for multiple includes Fixes delay initial value bug

--- GH-MicroBlaze/axi_0/src/options.h
+++ GH-MicroBlaze/axi_0/src/options.h
@@ -1,6 +1,3 @@
-#ifndef OPTIONS_H_
-#define OPTIONS_H_
-
 // Set current game to 'Rock Band 3' (RB) or 'Guitar Hero: Warriors of Rock' (GH)
 #define RB 0
 #define GH 1
@@ -8,5 +5,3 @@
 
 // Enable (1) or disable (0) image filters
 #define FILTERS 0
-
-#endif /* OPTIONS_H_ */
